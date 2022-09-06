249bca6238e519b0234d950f474b4b2fd67d4fa9
Josep <josepcortes6@gmail.com>
2022-09-06 19:43:45 +0200

    WIP

--- routes/training-routes.js
+++ routes/training-routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const Training = require('../models/Training');
-const training = require('../models/Training');
+
+
 
 router.get('/', async(req, res, next) => {
     try {
@@ -12,5 +13,24 @@ router.get('/', async(req, res, next) => {
     catch (error) {
         console.error(error)
         next(error)
+    }
+});
+
+router.post('/', async(req,res,next) => {
+    const {name, image, date, category} = req.body
+    const dateParsed = parseInt(date)
+    
+    try {
+        const newtraining= {name, image, dateParsed, category}
+        const training= await Training.create(newtraining)
+        if (!training) {
+            next(new ErrorResponse('An error ocurred while creating training', 500));
+          }
+          res.status(201).json({ data: training})
+
+    } catch (error) {
+        console.error(error)
+        next(error)
+        
     }
 });
