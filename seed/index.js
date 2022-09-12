9f1fa7d91597d59d0eb4d854512a38200da4c455
Josep <josepcortes6@gmail.com>
2022-09-12 19:25:00 +0200

    WIP

--- seed/index.js
+++ seed/index.js
@@ -3,16 +3,16 @@ const mongoose = require('mongoose');
 const Training = require('../models/Training')
 
 const trainings = [
-  {},
-  {},
-  {},
-  {}
+  {name:"Paos", image:"/images/paos.jpeg",date:"10-10-2022",category:'pads'},
+  {name:"Airbike", image:"/images/airbike.jpeg",date:"10-10-2022",category:'airbike'},
+  {name:"Tabata", image:"/images/tabata.jpeg",date:"12-10-2022",category:'tabata'},
+  {name:"Sparring", image:"/images/sparring.jpeg",date:"11-10-2022",category:'sparring'}
 ]
 
 mongoose.connect(process.env.MONGO_URL)
   .then(x => console.log(`Connected to ${x.connection.name}`))
   .then(() => {
-    return // Model.create(array)
+    return Training.create(trainings)
   })
   .then(() => {
     console.log('Seed done 🌱');
